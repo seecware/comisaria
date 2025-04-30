f50ba0490cd43832e98ce0ba80565642f358d147
Francisco Mendoza <seecware@gmail.com>
2025-04-30 00:26:45 -0600

    Adding partials folder an files...

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!-- includes/header.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-100 text-gray-800">
+  <header class="bg-white shadow">
+    <div class="h-16 max-w-7xl mx-auto p-4 flex justify-between items-center">
+        <div class="flex h-full items-end">
+            <img class="object-contain h-full"  src="/assets/logo.jpg" alt="Logo">
+            <h1 class="text-xl ml-2 font-bold">Comisaria</h1>
+        </div>
+      <nav>
+        <a href="/index.php" class="text-blue-600 hover:underline mr-4">Inicio</a>
+        <a href="/pages/contacto.php" class="text-blue-600 hover:underline">Contacto</a>
+      </nav>
+    </div>
+  </header>
+</html>
---
+++ includes/layout.php
@@ -0,0 +1,7 @@
+<?php include __DIR__ . '/header.php'; ?>
+
+<main class="max-w-4xl mx-auto mt-10 h-screen">
+  <?php include $parentContent; ?>
+</main>
+
+<?php include __DIR__ . '/footer.php'; ?>
