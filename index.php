244450e0b5072457b54f47dd8ea3d83a4a68d7fe
Francisco Mendoza <seecware@gmail.com>
2025-04-30 00:19:55 -0600

    Deleting harcoded html from index.php And bringing router from another file to slice all programs into functionalities.

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
-<html>
-  <head>
-    <title>your_domain website</title>
-  </head>
-  <body>
-    <h1>Hello World!</h1>
-
-    <p>This is the landing page of <strong>your_domain</strong>.</p>
-  </body>
-</html>
-
 <?php
-$user = "comisario";
-$password = "F813BD3EED";
-$database = "comisaria";
-$table = "payments";
-
-try {
-  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-  echo "<h2>TODO</h2><ol>";
-  foreach($db->query("SELECT * FROM $table") as $row) {
-	  echo "<li>" ."$" .$row['ammount'] ."-". $row['year'] .$row .  "</li>";
-	  print_r($row);
-  }
-  echo "</ol>";
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require 'routes.php';
